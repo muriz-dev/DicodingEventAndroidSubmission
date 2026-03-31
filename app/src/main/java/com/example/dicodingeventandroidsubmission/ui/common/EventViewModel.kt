53e3c4f3b1ad123b54260c27d87db6a5aee3ba48
muriz.dev <mhmdrizki.dev@gmail.com>
2026-03-31 19:41:23 +0700

    Implement event repository

--- app/src/main/java/com/example/dicodingeventandroidsubmission/ui/common/EventViewModel.kt
+++ app/src/main/java/com/example/dicodingeventandroidsubmission/ui/common/EventViewModel.kt
@@ -1,54 +1,10 @@
 package com.example.dicodingeventandroidsubmission.ui.common
 
-import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.dicodingeventandroidsubmission.data.response.EventListResponse
-import com.example.dicodingeventandroidsubmission.data.response.ListEventsItem
-import com.example.dicodingeventandroidsubmission.data.retrofit.ApiConfig
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.example.dicodingeventandroidsubmission.data.repository.EventsRepository
 
-class EventViewModel(private val active: Int) : ViewModel() {
-    companion object {
-        private const val TAG = "EventViewModel"
-    }
+class EventViewModel(private val eventsRepository: EventsRepository) : ViewModel() {
+    fun getEvents(activeStatus: Int) = eventsRepository.getEvents(activeStatus)
 
-    private val _eventList = MutableLiveData<List<ListEventsItem>>()
-    val eventList: LiveData<List<ListEventsItem>> = _eventList
-
-    private val _isLoading = MutableLiveData<Boolean>()
-    val isLoading: LiveData<Boolean> = _isLoading
-
-    init {
-        getEventList(active)
-    }
-
-    private fun getEventList(activeStatus: Int) {
-        _isLoading.value = true
-        val client = ApiConfig.getApiService().getEventList(activeStatus)
-
-        client.enqueue(object : Callback<EventListResponse> {
-            override fun onResponse(
-                call: Call<EventListResponse>,
-                response: Response<EventListResponse>
-            ) {
-                _isLoading.value = false
-                if (response.isSuccessful) {
-                    _eventList.value = response.body()?.listEvents
-
-                    Log.d(TAG, "onResponse: ${response.body()?.listEvents?.count()}")
-                } else {
-                    Log.e(TAG, "onFailure: ${response.message()}")
-                }
-            }
-
-            override fun onFailure(call: Call<EventListResponse>, t: Throwable) {
-                _isLoading.value = false
-                Log.e(TAG, "onFailure: ${t.message.toString()}")
-            }
-        })
-    }
+    fun getFavoriteEvents() = eventsRepository.getFavoriteEvents()
 }
--- app/src/main/java/com/example/dicodingeventandroidsubmission/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/dicodingeventandroidsubmission/ui/home/HomeViewModel.kt
@@ -4,86 +4,20 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.dicodingeventandroidsubmission.data.response.EventListResponse
-import com.example.dicodingeventandroidsubmission.data.response.ListEventsItem
-import com.example.dicodingeventandroidsubmission.data.retrofit.ApiConfig
+import com.example.dicodingeventandroidsubmission.data.remote.response.EventListResponse
+import com.example.dicodingeventandroidsubmission.data.remote.response.ListEventsItem
+import com.example.dicodingeventandroidsubmission.data.remote.retrofit.ApiConfig
+import com.example.dicodingeventandroidsubmission.data.repository.EventsRepository
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel(private val eventsRepository: EventsRepository) : ViewModel() {
     companion object {
         private const val TAG = "HomeViewModel"
     }
 
-    private val _upcomingEvents = MutableLiveData<List<ListEventsItem>>()
-    val upcomingEvents: LiveData<List<ListEventsItem>> = _upcomingEvents
+    fun getUpcomingEvents() = eventsRepository.getEvents(1)
 
-    private val _finishedEvents = MutableLiveData<List<ListEventsItem>>()
-    val finishedEvents: LiveData<List<ListEventsItem>> = _finishedEvents
-
-    private val _isLoading = MutableLiveData<Boolean>()
-    val isLoading: LiveData<Boolean> = _isLoading
-
-    private var isUpcomingRunning = false
-    private var isFinishedRunning = false
-
-    init {
-        loadAllEvents()
-    }
-
-    private fun loadAllEvents() {
-        getUpcomingEvents()
-        getFinishedEvents()
-    }
-
-    private fun getUpcomingEvents() {
-        isUpcomingRunning = true
-        _isLoading.value = true
-
-        val client = ApiConfig.getApiService().getEventList(1)
-        client.enqueue(object : Callback<EventListResponse> {
-            override fun onResponse(call: Call<EventListResponse>, response: Response<EventListResponse>) {
-                isUpcomingRunning = false
-                checkLoadingStatus()
-
-                if (response.isSuccessful) {
-                    _upcomingEvents.value = response.body()?.listEvents
-                }
-            }
-
-            override fun onFailure(call: Call<EventListResponse>, t: Throwable) {
-                isUpcomingRunning = false
-                checkLoadingStatus()
-                Log.e(TAG, "onFailure Upcoming: ${t.message}")
-            }
-        })
-    }
-
-    private fun getFinishedEvents() {
-        isFinishedRunning = true
-        _isLoading.value = true
-
-        val client = ApiConfig.getApiService().getEventList(0)
-        client.enqueue(object : Callback<EventListResponse> {
-            override fun onResponse(call: Call<EventListResponse>, response: Response<EventListResponse>) {
-                isFinishedRunning = false
-                checkLoadingStatus()
-
-                if (response.isSuccessful) {
-                    _finishedEvents.value = response.body()?.listEvents
-                }
-            }
-
-            override fun onFailure(call: Call<EventListResponse>, t: Throwable) {
-                isFinishedRunning = false
-                checkLoadingStatus()
-                Log.e(TAG, "onFailure Finished: ${t.message}")
-            }
-        })
-    }
-
-    private fun checkLoadingStatus() {
-        _isLoading.value = isUpcomingRunning || isFinishedRunning
-    }
+    fun getFinishedEvents() = eventsRepository.getEvents(0)
 }
