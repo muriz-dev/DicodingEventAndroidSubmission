f1d570a2f3338bcdf0cdc8b2000794f17703e84f
muriz.dev <mhmdrizki.dev@gmail.com>
2026-03-31 19:41:45 +0700

    Make sure one instance created

--- app/src/main/java/com/example/dicodingeventandroidsubmission/ui/common/EventViewModelFactory.kt
+++ app/src/main/java/com/example/dicodingeventandroidsubmission/ui/common/EventViewModelFactory.kt
@@ -1,13 +1,25 @@
 package com.example.dicodingeventandroidsubmission.ui.common
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.dicodingeventandroidsubmission.data.repository.EventsRepository
+import com.example.dicodingeventandroidsubmission.di.Injection
 
-class EventViewModelFactory(private val active: Int) : ViewModelProvider.Factory {
+class EventViewModelFactory(private val eventsRepository: EventsRepository) : ViewModelProvider.Factory {
+    companion object {
+        @Volatile
+        private var instance: EventViewModelFactory? = null
+        fun getInstance(context: Context): EventViewModelFactory =
+            instance ?: synchronized(this) {
+                instance ?: EventViewModelFactory(Injection.provideRepository(context))
+            }.also { instance = it }
+    }
+
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(EventViewModel::class.java)) {
-            return EventViewModel(active) as T
+            return EventViewModel(eventsRepository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
     }
