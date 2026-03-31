a342f404083bf0d65cf5949d31e80e84453cdee0
muriz.dev <mhmdrizki.dev@gmail.com>
2026-03-31 19:37:06 +0700

    Move to Remote package

--- app/src/main/java/com/example/dicodingeventandroidsubmission/data/remote/response/EventListResponse.kt
+++ app/src/main/java/com/example/dicodingeventandroidsubmission/data/remote/response/EventListResponse.kt
@@ -1,4 +1,4 @@
-package com.example.dicodingeventandroidsubmission.data.response
+package com.example.dicodingeventandroidsubmission.data.remote.response
 
 import com.google.gson.annotations.SerializedName
 
