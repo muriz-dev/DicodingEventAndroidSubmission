b190015179a9bb6c96600af2fb369042eba19cc2
muriz.dev <mhmdrizki.dev@gmail.com>
2026-03-31 19:39:21 +0700

    Add view model for detail

--- app/src/main/java/com/example/dicodingeventandroidsubmission/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/dicodingeventandroidsubmission/ui/detail/DetailViewModel.kt
@@ -1,49 +1,27 @@
 package com.example.dicodingeventandroidsubmission.ui.detail
 
-import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.dicodingeventandroidsubmission.data.response.Event
-import com.example.dicodingeventandroidsubmission.data.response.EventDetailResponse
-import com.example.dicodingeventandroidsubmission.data.retrofit.ApiConfig
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import androidx.lifecycle.viewModelScope
+import com.example.dicodingeventandroidsubmission.data.local.entity.EventsEntity
+import com.example.dicodingeventandroidsubmission.data.repository.EventsRepository
+import kotlinx.coroutines.launch
 
-class DetailViewModel : ViewModel() {
+class DetailViewModel(private val eventsRepository: EventsRepository) : ViewModel() {
     companion object {
         private const val TAG = "DetailViewModel"
     }
 
-    private val _eventDetail = MutableLiveData<Event>()
-    val eventDetail: LiveData<Event> = _eventDetail
-    private val _isLoading = MutableLiveData<Boolean>()
-    val isLoading: LiveData<Boolean> = _isLoading
-    private val _errorMessage = MutableLiveData<String>()
-    val errorMessage: LiveData<String> = _errorMessage
+    fun getEventDetail(eventId: Int) = eventsRepository.getEventDetail(eventId)
 
-    fun getEventDetail(id: String) {
-        _isLoading.value = true
-        val client = ApiConfig.getApiService().getEventDetail(id)
-        client.enqueue(object : Callback<EventDetailResponse> {
-            override fun onResponse(call: Call<EventDetailResponse>, response: Response<EventDetailResponse>) {
-                _isLoading.value = false
-                if (response.isSuccessful) {
-                    _eventDetail.value = response.body()?.event
+    fun saveToFavorite(event: EventsEntity) {
+        viewModelScope.launch {
+            eventsRepository.setEventsFavorite(event, true)
+        }
+    }
 
-                    Log.d(TAG, "onResponse: ${response.body()?.event}")
-                } else {
-                    _errorMessage.value = response.message()
-                }
-            }
-
-            override fun onFailure(call: Call<EventDetailResponse>, t: Throwable) {
-                _isLoading.value = false
-                _errorMessage.value = t.message
-
-                Log.e(TAG, "onFailure: ${t.message.toString()}")
-            }
-        })
+    fun deleteFromFavorite(event: EventsEntity) {
+        viewModelScope.launch {
+            eventsRepository.setEventsFavorite(event, false)
+        }
     }
 }
