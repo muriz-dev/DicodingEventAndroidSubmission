65ceee43d32d117497ca82d9c53571769a87669e
muriz.dev <mhmdrizki.dev@gmail.com>
2026-04-01 07:13:25 +0700

    Add get event search with query

--- app/src/main/java/com/example/dicodingeventandroidsubmission/data/remote/retrofit/ApiService.kt
+++ app/src/main/java/com/example/dicodingeventandroidsubmission/data/remote/retrofit/ApiService.kt
@@ -2,7 +2,6 @@ package com.example.dicodingeventandroidsubmission.data.remote.retrofit
 
 import com.example.dicodingeventandroidsubmission.data.remote.response.EventDetailResponse
 import com.example.dicodingeventandroidsubmission.data.remote.response.EventListResponse
-import retrofit2.Call
 import retrofit2.http.*
 
 interface ApiService {
@@ -15,4 +14,10 @@ interface ApiService {
     suspend fun getEventDetail(
         @Path("id") id: String
     ): EventDetailResponse
+
+    @GET("events")
+    suspend fun getEventSearch(
+        @Query("active") active: Int = -1,
+        @Query("q") query: String
+    ): EventListResponse
 }
