f46606d4cddf8ca04532fc889e01c5c55a461eb7
muriz.dev <mhmdrizki.dev@gmail.com>
2026-04-01 07:16:48 +0700

    Remove redundant on private constructor

--- app/src/main/java/com/example/dicodingeventandroidsubmission/data/Result.kt
+++ app/src/main/java/com/example/dicodingeventandroidsubmission/data/Result.kt
@@ -1,6 +1,6 @@
 package com.example.dicodingeventandroidsubmission.data
 
-sealed class Result<out R> private constructor() {
+sealed class Result<out R> {
     data class Success<out R>(val value: R) : Result<R>()
     data class Error(val error: String) : Result<Nothing>()
     object Loading : Result<Nothing>()
